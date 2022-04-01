e1d431f4754f80a428a7fdc3924e1297c7487b48
colette dufficy <c.dufficy@gmail.com>
2022-04-01 21:47:26 +0100

    extensions started

--- src/room.py
+++ src/room.py
@@ -4,42 +4,39 @@ class Room:
         self.room_name = room_name
         self.max_capacity = max_capacity
         self.till = till
-        self.number_of_guests = []
+        self.list_of_guests = []
         self.playlist = []
         
         
-    def guest_count(self):
-        return len(self.number_of_guests)
+    
+    def head_count(self):
+        return len(self.list_of_guests)
         
     def check_in(self, guest):
-        self.number_of_guests.append(guest)
+        self.list_of_guests.append(guest)
+        self.max_capacity -= 1
 
     def check_out(self, guest):
-        self.number_of_guests.remove(guest)
-            
+        self.list_of_guests.remove(guest)
+        self.max_capacity += 1
+
     def add_song_to_playlist(self, song):
         self.playlist.append(song)
         
-    def check_room_capacity(self, guest):
-        if len(self.number_of_guests) < self.max_capacity:
-            self.check_in(guest)
-            return self.guest_count()
+    def room_has_capacity(self, guest):
+        if len(self.list_of_guests) < self.max_capacity:
+            self.list_of_guests.append(guest)
+            return "come on in"
+        else:
+            return f"Sorry, the {self.room_name} is full"
     
     
         # else:
         #     return f"Sorry, the {self.room_name} is full"
         
+            #     else:
+            # return "oops"
+        
               
+     
         
-        # if the len of number of guests less than max cpaity interger, then the room is not full
-        # add guest
-        # else
-        # return a string - sorry room is full
-        # breakpoint()
-        
-        
-        
-        
-        # is the len of number of guests == max cpaity interger, - true or false
-        # if true, no more guests
-        # if false, add a guest
--- tests/room_test.py
+++ tests/room_test.py
@@ -6,13 +6,13 @@ from src.song import Song
 class TestRoom(unittest.TestCase):
     
     def setUp(self):
-        self.room1 = Room("Blue Room", 7, 100)
-        self.room2 = Room("Green Room", 3, 50)
-        
         self.guest1 = Guest("Winkie", 50, "Wannabe")
         self.guest2 = Guest("Leigh", 40, "Business Time")
         self.guest3 = Guest("Becky", 20, "Let it Go")
         self.guest4 = Guest("Erica", 5, "Maneater")
+        
+        self.room1 = Room("Blue Room", 7, 100)
+        self.room2 = Room("Green Room", 3, 50)
         
         self.song1 = Song("Spice Girls", "Wannabe")
         self.song2 = Song("Mr Brightside", "The Killers")
@@ -27,31 +27,52 @@ class TestRoom(unittest.TestCase):
         self.assertEqual(50, self.room2.till)
         
     def test_number_of_guests_in_empty_room(self):
-        self.assertEqual(0, self.room1.guest_count())
+        self.assertEqual(0, self.room1.head_count())
         
     def test_check_in_guest(self):
         self.room1.check_in(self.guest1)
         self.room1.check_in(self.guest2)
         self.room1.check_in(self.guest3)
-        self.assertEqual(3, self.room1.guest_count())
+        self.assertEqual(3, self.room1.head_count())
         
     def test_check_out_guest(self):
         self.room1.check_in(self.guest1)
         self.room1.check_in(self.guest2)
         self.room1.check_out(self.guest2)
-        self.assertEqual(1, self.room1.guest_count())
+        self.assertEqual(1, self.room1.head_count())
     
     def test_add_song_to_playlist(self):
         self.room1.add_song_to_playlist(self.song1)
         self.assertEqual("Wannabe", self.song1.title)
         
-    def test_check_room_capacity__not_full(self):
-        # self.room2.check_in(self.guest1)
-    #     # self.room2.check_in(self.guest1)
-    #     # self.room2.check_in(self.guest1)
-    #     # self.room2.check_in(self.guest1)
+    # room2 starts with noone in the room, with a MAX capacity of 3 people.... 
+    def test_room_has_capacity1__there_is_room(self):
+        #this room_has_capacity function checks if there is space and adds 1 person if there is.. else says oops
+        self.assertEqual("come on in", self.room2.room_has_capacity(self))
+        #You can test multiple assertEquals to help testing phase, understand what multiple variables are doing
+        #after you run the room_has_capacity in this scenario 1 PERSON SHOULDDDD be added to this room meaning when you do head count it should return 1...
+        self.assertEqual(1, self.room2.head_count())
+        
+    def test_room_has_capacity2__there_is_room(self):
+        #add 2 people to the room if there is capacity which there should be...
+        self.room2.room_has_capacity(self)
+        self.assertEqual("come on in", self.room2.room_has_capacity(self))
+        self.assertEqual(2, self.room2.head_count())
 
-        self.assertEqual(1, self.room2.check_room_capacity(self.guest1))
+    def test_room_has_capacity3__there_is_room(self):
+        self.room2.room_has_capacity(self)
+        self.room2.room_has_capacity(self)
+        self.assertEqual("come on in", self.room2.room_has_capacity(self))
+        self.assertEqual(3, self.room2.head_count())
+        
+    def test_room_has_capacity4__there_is_no_room(self):
+        self.room2.room_has_capacity(self)
+        self.room2.room_has_capacity(self)
+        self.room2.room_has_capacity(self)
+        self.assertEqual(f"Sorry, the {self.room2.room_name} is full", self.room2.room_has_capacity(self))
+        self.assertEqual(3, self.room2.head_count())
+
+        
         
         
         
